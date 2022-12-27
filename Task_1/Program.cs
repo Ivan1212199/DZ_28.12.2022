ea1d6feeedcdb73b9bb2d9abac7f60de0fa411fe
Ivan <ivan18-18@example.com>
2022-12-27 21:13:18 +0300

    вроде готово

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом метод сравнения каждого символа.*/
+
+
+Console.Write("Введите пятизначное число: ");
+string numStr = Console.ReadLine()!;
+int firstNum = 0;
+int endNum = numStr.Length - 1;
+
+if (endNum == 4)
+{
+    while (firstNum < endNum && Char.IsDigit(numStr[firstNum]) && Char.IsDigit(numStr[endNum]))
+    {
+        if (numStr[firstNum] == numStr[endNum])
+        {
+            firstNum++;
+            endNum = endNum - 1;
+        }
+        else
+        {
+            Console.WriteLine($"{numStr[firstNum]} != {numStr[endNum]} : число {numStr} не палиндром");
+            firstNum = 5;
+        }
+    }
+if (firstNum == 2) Console.WriteLine($"число {numStr} - палиндром");
+if (firstNum != 2 && firstNum != 5) Console.WriteLine($"{numStr} - не верный ввод");
+}
+else Console.WriteLine("Значение не пятизначное");
+
--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ //Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+int a1, a2, a3;
+int b1, b2, b3;
+
+Console.WriteLine("Введите посследовательность координат: ");
+Console.Write("a1: ");
+a1 = Console.WriteLine(Console.ReadLine());
+Console.Write("a2: ");
+a2 = Console.WriteLine(Console.ReadLine());
+Console.Write("a3: ");
+a3 = Console.WriteLine(Console.ReadLine());
+
+Console.Write("b1: ");
+b1 = Console.ReadLine(Console.ReadLine());
+Console.Write("b2: ");
+b2 = Console.ReadLine(Console.ReadLine());
+Console.Write("b3: ");
+b3 = Console.ReadLine(Console.ReadLine());
+
+double resultDoub = Math.Pow(b1 - a1, 2) + Math.Pow(b2 - a2, 2) + Math.Pow(b3 - a3, 2);
+double resultDoub = Math.Sqrt(resultDoub);
+double resultDoub = Math.Round(resultDoub, 2);
+Console.WriteLine(resultDoub);
+
+/* я так и не нашел команды которые помагли б мне с решением этой задачи*/
